0881b182a6031321612dd814494373faf133dae0
richmondae <122391258+richmondae@users.noreply.github.com>
2023-09-30 14:39:32 +0100

    Update

--- lesson02/scripts/scripts.js
+++ lesson02/scripts/scripts.js
@@ -2,5 +2,7 @@ let current_date = new Date();
 let current_year = current_date.getFullYear();
 
 document.querySelector("span").textContent = current_year;
-
 document.getElementById("updated").textContent = "Last Updated: " + document.lastModified;
+
+let text = document.lastModified;
+            document.getElementById("currentdate").innerHTML = `Last Updated: <span id="currentdate">${text}</span>`
