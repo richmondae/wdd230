dd56bde586604f8cd23e1e95e4122f5a5c074bd9
richmondae <122391258+richmondae@users.noreply.github.com>
2023-10-08 13:34:33 +0100

    update

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -9,7 +9,24 @@ const currentDate = new Intl.DateTimeFormat("en-UK", {
   document.querySelector("#currentDate").innerHTML = currentDate;
   
   
+  /*Hamburger Button */
+  const hamButton = document.querySelector('#menu');
+  const navigation= document.querySelector('.navigation');
+  
+  hamButton.addEventListener('click', () => {
+      navigation.classList.toggle('open');
+      hamButton.classList.toggle('open');
+  });
   
   const date = new Date(document.lastModified).toLocaleString();
+  
   document.querySelector("#last-updated").innerHTML = date;
+  
+  const hero = new Date();
+  const day = hero.getDay();
+  if (day < 1 || day > 2) {
+    const x = document.querySelector('.topbar');
+    x.remove();
+  }
+  
   
