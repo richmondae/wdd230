652179b94cc53562f1bc2d27adadaf669cb6e971
richmondae <122391258+richmondae@users.noreply.github.com>
2023-10-10 16:45:21 +0100

    Book of Mormon Favorite Chapters Application

---
+++ lesson05/scripts/main.js
@@ -0,0 +1,27 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'X';
+  listBtn.style.fontWeight = 'bold';
+  listBtn.style.color = 'red';
+  list.appendChild(listItem);
+
+  listBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
+  });
+
+  input.focus();
+});
