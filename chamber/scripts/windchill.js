d153272bbbbc10107cfff9e55b2254b25259da23
richmondae <122391258+richmondae@users.noreply.github.com>
2023-11-21 19:26:18 +0100

    update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,62 +1,112 @@
-const tempInput = document.querySelector("#temp");
-const speedInput = document.querySelector("#speed");
-const windChillElement = document.querySelector("#windChill");
-
-function calculateWindChill() {
-    console.log("Function called");
-    const temperature = parseFloat(tempInput.value);
-    const windSpeed = parseFloat(speedInput.value);
-    console.log("Temperature:", temperature);
-    console.log("Wind Speed:", windSpeed);
-
-    if (!isNaN(temperature) && !isNaN(windSpeed)) {
-        if (temperature <= 50 && windSpeed > 3.0) {
-            const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-
-            windChillElement.textContent = `Wind Chill: ${windChill.toFixed(2)} °F`;
-
-        } else {
-            windChillElement.textContent = "Wind Chill: N/A";
-        }
-    }
-}
-
-tempInput.addEventListener("input", calculateWindChill);
-speedInput.addEventListener("input", calculateWindChill);
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=ebfd97e6480fd2f146e47e84870f7e7f";
+const apiURLW = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=ebfd97e6480fd2f146e47e84870f7e7f";
+const imgURL = 'https://openweathermap.org/img/w/'
 
 
+fetch(apiURLW)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 
 
-function loadDynamicModule(callback) {
-    const script = document.createElement('script');
-    script.src = 'dynamicModule.js';
-    script.onload = callback;
-    document.head.appendChild(script);
+    document.querySelector('#condition').textContent = jsObject.weather[0].description;
+    document.querySelector('#temp').innerHTML = jsObject.main.temp.toFixed(0) +'&deg;F';
+    document.querySelector('#humidity').textContent = jsObject.main.humidity + "%";
+    document.querySelector('#windspeed').textContent = jsObject.wind.speed.toFixed(0) + "mph";
+
+    let t = jsObject.main.temp;
+    let ws = jsObject.wind.speed;
+    
+    function windChill(tempF, speed) {
+      if (tempF <= 58 && speed > 3) {
+          let f = 35.74 + (0.6215 * tempF) - (35.75 * (speed ** 0.16)) + (0.4275 * tempF * (speed ** 0.16));
+          return f;
+      } else {
+          let f = "N/A";
+          return f;
+  
+      }
   }
+
+  let value = windChill(t, ws);
   
-  // Usage
-  document.getElementById('loadButton').addEventListener('click', () => {
-    loadDynamicModule(() => {
-      const dynamicModule = window.dynamicModule.default(); // Assuming the module exports a default function
-      console.log(dynamicModule);
-    });
+
+  document.getElementById('windchill').textContent = value;
+
   });
-  
 
 
 
 
 
 
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let day = 0;
+    
+
+    //Makes the list array 5
+    const fiveDayForecast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+    
+
+    console.log(fiveDayForecast);
+
+    fiveDayForecast.forEach(x => {
+      let d = new Date(x.dt_txt);
+
+      let topp = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
+      let desc = x.weather[0].description;
+
+
+      document.getElementById(`day${day+1}`).textContent = week[d.getDay()];
+      document.getElementById(`forecast${day+1}`).innerHTML = x.main.temp +'&deg;F';
+      
+      document.getElementById(`icon${day+1}`).setAttribute('src', topp);
+      document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+      day++;
+
+    });
+
+  });
 
 
 
+// PRESTON EVENTS
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.log(jsonObject);
+    const towns = jsonObject['towns'];
 
+    for (let i = 0; i < towns.length; i++) {
 
+      if (towns[i].name === "Preston") {
 
+        let event = document.createElement('section');
 
+        let first = document.createElement('p')
+        let second = document.createElement('p')
+        let third = document.createElement('p')
 
-const dynamicModule = await import('./dynamicModule.js');
-const result = dynamicModule.default(); // Assuming the module exports a default function
-console.log(result); // Output the result from the dynamically loaded module
+        first.textContent = `${towns[i].events[0]} `;
+        second.textContent = `${towns[i].events[1]} `;
+        third.textContent = `${towns[i].events[2]} `;
+
+        event.append(first);
+        event.append(second);
+        event.append(third)
+
+        document.querySelector('div.events').appendChild(event);
+      }
+
+    };
+
+  });
